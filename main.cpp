2139b7a96787ff2c5a4e2fdbac3a543df6648ddb
Hasib Ziai <16511616+HasibZiai@users.noreply.github.com>
2021-03-24 13:08:08 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,4 @@
-//File: main.cpp
-//Group Charlie's Members: Abhyuday Vatsavai, Duy Do, Hasibullah Ziai, Jason Lieu
-//Course: CPSC 254 Tue Thr 6:00
-//Start Date: 11/15/2017 -Duy Do
-//Date last Modified: 11/16/2017 12:03AM -Duy Do 
+
 
 #include <iostream>
 #include <fstream>
